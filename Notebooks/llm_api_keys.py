ed1f7a8154f41578ce4292cb4889ce510398a52d
MichaelWilson01 <mkw19c@my.fsu.edu>
2023-04-23 11:24:16 -0400

    update

--- Notebooks/llm_api_keys.py
+++ Notebooks/llm_api_keys.py
@@ -1,7 +1,7 @@
 import os
 
-api_key_list = os.listdir("C:\\Users\\micha\\Desktop\\LLM_api_keys\\")
+api_key_list = os.listdir("C:\\Users\\micha\\Documents\\GitHub\\Prompt-Engineering\\hidden_folder\\LLM_api_keys\\")
 
 for file in api_key_list:
-    with open('C:\\Users\\micha\\Desktop\\LLM_api_keys\\'+file) as f:
+    with open("C:\\Users\\micha\\Documents\\GitHub\\Prompt-Engineering\\hidden_folder\\LLM_api_keys\\"+file) as f:
         os.environ[file[:-4]] = f.readlines()[0].strip()
